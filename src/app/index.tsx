5eb11ac549e767b280bfa19f9ac841ed5d5b687e
DeVeStEr <dalex017@ucr.com>
2025-10-26 22:27:54 -0700

    Test idea for the Login Screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,82 @@
-import { View } from "react-native";
+import { View, StyleSheet, TextInput, Text, TouchableOpacity} from "react-native";
 import { AppText } from "@/components/AppText";
 
 export default function IndexScreen() {
   return (
     <View className="justify-center flex-1 p-4">
-      <AppText center>
-        Open up <AppText bold>app/index.tsx</AppText> to start working on your
-        app!
-      </AppText>
+      <View style={styles.background}> 
+        <Text style={styles.bigFont}>
+          R'ATE
+        </Text>
+        <View style={styles.BlueCenterBox}>
+          <View style={styles.LoginUsernameBox}>
+            <TextInput placeholder={" Username"}/>
+          </View>
+          <View style={styles.LoginPasswordBox}>
+            <TextInput placeholder={" Password"}/>
+          </View>
+          <TouchableOpacity style={styles.LoginSignInBox}>
+            <Text style={{textAlign: 'center', fontWeight: 'bold'}}>Sign In</Text>
+          </TouchableOpacity>
+          <Text style={{textAlign: 'center', fontWeight: 'bold'}}>or</Text>
+          <TouchableOpacity style={styles.LoginUCREmailBox}>
+            <Text style={{textAlign: 'center', fontWeight: 'bold'}}>Use with UCR Email</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  bigFont: {
+    fontSize: 48,
+    color:"#FFFB26"
+  },
+  background: {
+    flex: 1,
+    backgroundColor: "#120E8F",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  BlueCenterBox: {
+    width: 300,
+    height: 400,
+    backgroundColor: "#0000FF",
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  LoginUsernameBox: {
+    width: 250,
+    height: 20,
+    backgroundColor: "#FFFFFF",
+    alignSelf: 'center',
+    marginTop: 50,
+    borderRadius: 5,
+  },
+  LoginPasswordBox: {
+    width: 250,
+    height: 20,
+    backgroundColor: "#FFFFFF",
+    alignSelf: 'center',
+    marginTop: 30,
+    borderRadius: 5,
+  },
+  LoginSignInBox: {
+    width: 250,
+    height: 20,
+    backgroundColor: "#FFFB26",
+    alignSelf: 'center',
+    marginTop: 60,
+    marginBottom: 30,
+    borderRadius: 5,
+  },
+  LoginUCREmailBox: {
+    width: 250,
+    height: 20,
+    backgroundColor: "#FFFB26",
+    alignSelf: 'center',
+    marginTop: 30,
+    borderRadius: 5,
+  }
+});
