7f2b09ac7eb71caff8e7d79b9782f416357babee
DeVeStEr <dalex017@ucr.com>
2025-11-05 14:32:14 -0800

    reformatted the profile picture

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -11,89 +11,79 @@ export default function ProfileScreen() {
     }
 
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <ProfilePic width={100} height={100} />
-      <AppText size="large" bold>
-        John Doe 
-      </AppText>
-      <AppText size="medium">
-        Jdoe002@ucr.edu 
-      </AppText>
-      <AppText size="medium" bold>
-        Reviews              Likes
-      </AppText>
-      <View style={styles.BlueCenterBox}> 
-        <Text style={{alignSelf:"center", alignItems:"center", justifyContent:"center"}}>
-            Rank Level
-          </Text>
+    <View className="flex-1 justify-center items-center bg-white p-10">
+      <View className="mt-10"/>
+      <ProfilePic className="w-[50px] h-[100px]"/>
+      <Text className="text-3xl text-black font-bold p-3 tracking-wide">
+        John Doe
+      </Text>
+      <Text className="text-base text-black mb-2">
+        jdoe002@ucr.edu
+      </Text>
+      <View className="justify-center items-center">
+        <Text className="text-base text-black font-bold">
+          20                       100
+        </Text>
+        <Text className="text-base text-black">
+          Reviews                Likes
+        </Text>
       </View>
-      <ScrollView style={{height:200, flexGrow: 0}}>
-        <View style={styles.ReviewBox}> 
-          <Text style={{alignSelf:"center", alignItems:"center", justifyContent:"center"}}>
+      <View className="w-full h-10 flex-row rounded-lg justify-center items-center mt-3 mb-2">
+        <TouchableOpacity className="w-[150px] h-6 bg-[#011A69] rounded-lg justify-center items-center mr-3" onPress={navigate}>
+          <Text className="text-base text-white font-bold">
+            Sign Out
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="w-[150px] h-6 bg-[#011A69] rounded-lg justify-center items-center" onPress={navigate}>
+          <Text className="text-base text-white font-bold">
+            Edit Profile
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View className="w-full h-[100px] bg-[#295298] rounded-lg mt-2 p-2">
+        <Text className="text-base text-white font-bold ml-2">
+          * Rank Level *
+        </Text>
+      </View>
+      <View className="w-full h-10 bg-[#A1B1E4] flex-row rounded-full justify-center items-center mt-3 mb-2">
+        <TouchableOpacity className="w-[90px] h-6 bg-white rounded-full justify-center items-center mr-4">
+          <Text className="text-base text-black font-bold">
+            Reviews
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="w-[90px] h-6 bg-white rounded-full justify-center items-center mr-4">
+          <Text className="text-base text-black font-bold">
+            Badges
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="w-[90px] h-6 bg-white rounded-full justify-center items-center">
+          <Text className="text-base text-black font-bold">
+            Favorites
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView className="h-full grow-0 w-full">
+        <View className="w-full h-[120px] bg-[#C2D0FF] rounded-xl justify-center items-center mb-6">
+          <Text className="text-base text-black font-bold">
             Review
           </Text>
         </View>
-        <View style={styles.ReviewBox}> 
-          <Text style={{alignSelf:"center", alignItems:"center", justifyContent:"center"}}>
+        <View className="w-full h-[120px] bg-[#C2D0FF] rounded-xl justify-center items-center mb-6">
+          <Text className="text-base text-black font-bold">
             Review
           </Text>
         </View>
-        <View style={styles.ReviewBox}> 
-          <Text style={{alignSelf:"center", alignItems:"center", justifyContent:"center"}}>
+        <View className="w-full h-[120px] bg-[#C2D0FF] rounded-xl justify-center items-center mb-6">
+          <Text className="text-base text-black font-bold">
             Review
           </Text>
         </View>
-        <View style={styles.ReviewBox}>
-          <Text style={{alignSelf:"center", alignItems:"center", justifyContent:"center"}}>
+        <View className="w-full h-[120px] bg-[#C2D0FF] rounded-xl justify-center items-center mb-6">
+          <Text className="text-base text-black font-bold">
             Review
           </Text> 
         </View>
       </ScrollView>
-      <TouchableOpacity style={styles.SignOutBox} onPress={navigate}>
-        <Text style={{textAlign: 'center', fontWeight: 'bold'}}> 
-          Sign Out
-        </Text>
-      </TouchableOpacity>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  BlueCenterBox: {
-      width: 300,
-      height: 100,
-      backgroundColor: "#2727bfff",
-      alignSelf: 'center',
-      alignItems: "center",
-      marginBottom: 20,
-      borderRadius: 10,
-    },
-  ReviewBox: {
-      width: 300,
-      height: 100,
-      backgroundColor: "#7070e1ff",
-      marginBottom: 10,
-      borderRadius: 10,
-    },
-  fixedBox: {
-      width: 200,
-      height: 100,
-  },
-  BlueBox: {
-      width: 300,
-      height: 50,
-      backgroundColor: "#2727bfff",
-      alignSelf: 'center',
-      alignItems: "center",
-      marginBottom: 20,
-      borderRadius: 10,
-    },
-  SignOutBox: {
-    width: 250,
-    height: 20,
-    backgroundColor: "#FFFB26",
-    alignSelf: 'center',
-    marginTop: 20,
-    marginBottom: 30,
-    borderRadius: 5,
-  },
-});
